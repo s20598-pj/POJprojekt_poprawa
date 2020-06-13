2de6b20c9e1359f95618a20c3b11634d6103cc40
s20598-pj <56222968+s20598-pj@users.noreply.github.com>
2020-06-13 16:38:12 +0200

    Last commit

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 public class Service {
 
     Shop market;
@@ -7,5 +9,9 @@ public class Service {
         this.market = sh;
         this.products = pr;
         this.market.setList_avalibe_product(products);
+    }
+
+//    private Optional<Product> findProductInShop(final int productId) {
+//        return products.stream().filter(products -> products.getId() == productId).findFirst(); //coś mi sięnie udało :(
     }
 }
